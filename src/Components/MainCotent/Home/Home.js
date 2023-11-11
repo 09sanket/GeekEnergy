8f9d0f26768c23014d311439c92f653c4eeb2323
Sanket Rahanghdale <87478376+09sanket@users.noreply.github.com>
2023-11-11 23:14:31 +0530

    Update Home.js

--- src/Components/MainCotent/Home/Home.js
+++ src/Components/MainCotent/Home/Home.js
@@ -5,42 +5,45 @@ import DataContext from "../../../store/data-context";
 const MovieList = (props) => {
   const dataContext = useContext(DataContext);
 
-  const fetchData = async () => {
-    try {
-      dataContext.setFetchingData("Fetching data from server....");
-      const obj = {
-        category: "movies",
-        language: "Kannad",
-        genre: "all",
-        sort: "voting",
-      };
+ const fetchData = async () => {
+  try {
+    dataContext.setFetchingData("Fetching data from server....");
+    const obj = {
+      category: "movies",
+      language: "Kannad",
+      genre: "all",
+      sort: "voting",
+    };
 
-      const response = await fetch("https://hoblist.com/api/movieList", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(obj),
-      });
+    const response = await fetch("https://hoblist.com/api/movieList", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Add the following line:
+        "Access-Control-Allow-Origin": "https://09sanket.github.io",
+      },
+      body: JSON.stringify(obj),
+    });
 
-      const result = await response.json();
+    const result = await response.json();
 
-      if (result.error) {
-        let errorMessage = "Something went wrong!...";
-        if (result.error && result.error.message) {
-          errorMessage = result.error.message;
-        }
-        throw new Error(errorMessage);
-      } else {
-        console.log(result.result[20]);
-        dataContext.setMoviesList(result.result);
+    if (result.error) {
+      let errorMessage = "Something went wrong!...";
+      if (result.error && result.error.message) {
+        errorMessage = result.error.message;
       }
-    } catch (err) {
-      alert(err.message);
-    } finally {
-      dataContext.setFetchingData("");
+      throw new Error(errorMessage);
+    } else {
+      console.log(result.result[20]);
+      dataContext.setMoviesList(result.result);
     }
-  };
+  } catch (err) {
+    alert(err.message);
+  } finally {
+    dataContext.setFetchingData("");
+  }
+};
+
 
   useEffect(() => {
     fetchData();
