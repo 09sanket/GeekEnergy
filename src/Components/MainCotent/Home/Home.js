506273af4dbd79bdb6cc9ff0cf5873d95631b1b3
Sanket Rahanghdale <87478376+09sanket@users.noreply.github.com>
2023-11-11 23:35:00 +0530

    Update Home.js

--- src/Components/MainCotent/Home/Home.js
+++ src/Components/MainCotent/Home/Home.js
@@ -2,48 +2,45 @@ import React, { useContext, useEffect } from "react";
 import Movies from "./Movies";
 import DataContext from "../../../store/data-context";
 
-const MovieList = (props) => {
+const Home = (props) => {
   const dataContext = useContext(DataContext);
 
- const fetchData = async () => {
-  try {
-    dataContext.setFetchingData("Fetching data from server....");
-    const obj = {
-      category: "movies",
-      language: "Kannad",
-      genre: "all",
-      sort: "voting",
-    };
+  const fetchData = async () => {
+    try {
+      dataContext.setFetchingData("Fetching data from server....");
+      const obj = {
+        category: "movies",
+        language: "Kannad",
+        genre: "all",
+        sort: "voting",
+      };
 
-    const response = await fetch("https://hoblist.com/api/movieList", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Add the following line:
-        "Access-Control-Allow-Origin": "https://09sanket.github.io",
-      },
-      body: JSON.stringify(obj),
-    });
+      const response = await fetch("https://cors-anywhere.herokuapp.com/https://hoblist.com/api/movieList", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      });
 
-    const result = await response.json();
+      const result = await response.json();
 
-    if (result.error) {
-      let errorMessage = "Something went wrong!...";
-      if (result.error && result.error.message) {
-        errorMessage = result.error.message;
+      if (result.error) {
+        let errorMessage = "Something went wrong!...";
+        if (result.error && result.error.message) {
+          errorMessage = result.error.message;
+        }
+        throw new Error(errorMessage);
+      } else {
+        console.log(result.result[20]);
+        dataContext.setMoviesList(result.result);
       }
-      throw new Error(errorMessage);
-    } else {
-      console.log(result.result[20]);
-      dataContext.setMoviesList(result.result);
+    } catch (err) {
+      alert(err.message);
+    } finally {
+      dataContext.setFetchingData("");
     }
-  } catch (err) {
-    alert(err.message);
-  } finally {
-    dataContext.setFetchingData("");
-  }
-};
-
+  };
 
   useEffect(() => {
     fetchData();
@@ -51,15 +48,9 @@ const MovieList = (props) => {
 
   return (
     <>
-      {/* Your previous code */}
-      <div>
-        {/* Add any additional components or JSX as needed */}
-      </div>
-
-      {/* Render your component here */}
       <Movies />
     </>
   );
 };
 
-export default MovieList;
+export default Home;
